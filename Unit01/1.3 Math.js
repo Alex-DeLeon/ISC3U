7965413f6e46709f3bdcd6eed00b0ef574f3cb7e
Alex-DeLeon <159183729+Alex-DeLeon@users.noreply.github.com>
2024-04-04 14:21:55 -0400

    Update 1.3 Math.js

--- Unit01/1.3 Math.js
+++ Unit01/1.3 Math.js
@@ -18,10 +18,10 @@ console.log(fahrenheit + " degrees Fahrenheit is " + celsius + " degrees Celsius
 
 // Part 3: Slope of a Line
 
-let x1 = -4;
-let y1 = 5;
-let x2 = 6;
-let y2 = 0;
+let x1 = -4
+let y1 = 5
+let x2 = 6
+let y2 = 0
 
 let m = (y2 - y1)/(x2 - x1);
 
